67d54c0f7135ae6fe762ae1c21a9a320b3bccc70
bruce <bruceprk@hotmail.com>
2017-05-08 17:41:05 -0400

    mysql connection changed

--- bin/dbconnector.js
+++ bin/dbconnector.js
@@ -5,11 +5,11 @@
 var mysql = require('mysql');
 
 var dbcon = mysql.createConnection({
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
     user: 'eng',
     password: '3237a594e58baced6bd677a9431b7cda',
     database: 'engmem',
-    // socketPath: '/cloudsql/english-memorizer:us-central1:english-memorizer-db'
+    socketPath: '/cloudsql/english-memorizer:us-central1:english-memorizer-db'
 });
 
 dbcon.connect(function (err) {
